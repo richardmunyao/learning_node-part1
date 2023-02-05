b7114a4b1695c7ee311637b05c30084762b2beee
Richard Munyao <richard.munyao@gmail.com>
2023-02-05 18:52:15 +0100

    Integrating mongoose

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 import './index.css'
 
 // v1 of writing a promise
-const promise = axios.get('http://localhost:3001/notes')
-promise.then(response => {
-    // console.log(response)
-})
+// const promise = axios.get('http://localhost:3001/api/notes')
+// promise.then(response => {
+//     // console.log(response)
+// })
 
 //v2 of writing a promise
-axios
-    .get('http://localhost:3001/notes')
-    .then(response =>{
-        const notes = response.data
-        // console.log(notes)
-    })
+// axios
+//     .get('http://localhost:3001/api/notes')
+//     .then(response =>{
+//         const notes = response.data
+//         // console.log(notes)
+//     })
 
 
 const notes = [
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,6 @@
 //logic that holds connections to backend server
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {    
     const request = axios.get(baseUrl)
